da6cdd05d9b3ee67fc88e2f9a70d798aca8de022
Mandy Liu <manling512@gmail.com>
2023-07-04 22:33:44 +0000

    A function checks if two objects are equal.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -4,7 +4,7 @@ const eqObjects = function (object1, object2) {
 
   if (keys1.length !== keys2.length) {
     return false;
-  }
+  };
 
   for (const key of keys1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
