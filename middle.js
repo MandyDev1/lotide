3d0fb8b20a6c858b6427b909e1ee847ebb90ff55
Mandy Liu <manling512@gmail.com>
2023-07-06 15:46:27 +0000

    A function to return the middle-most element(s) of the given array.

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log("✅✅✅ Assertion Passed: Arrays are equal");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: Arrays are not equal");
-  }
-};
-
 const middle = function(array) {
   const length = array.length;
   if (length <= 2) {
@@ -25,17 +5,11 @@ const middle = function(array) {
   }
   if (length % 2 === 0) {
     const midIndex = length / 2 - 1;
-    return [array[midIndex], array[midIndex] - 1];
+    return [array[midIndex], array[midIndex] + 1];
   } else {
     const midIndex = Math.floor(length / 2);
     return [array[midIndex]];
   }
 }
 
-// Test cases
-assertArraysEqual(middle([1]), []); // Empty array for one element
-assertArraysEqual(middle([1, 2]), []); // Empty array for two elements
-assertArraysEqual(middle([1, 2, 3]), [2]); // Single middle element for odd number of elements
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Single middle element for odd number of elements
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Two middle elements for even number of elements
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Two middle elements for even number of elements
+module.exports = middle;
